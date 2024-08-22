41a89d26750f2371a45e493c08d0db9e0c238074
Sujoy Manir <sujoymanir@gmail.com>
2024-08-21 22:46:15 -0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
   return (
     <>
-      <div>
-     
-      </div>
-      
-
-    
-      </div>
-    
+      <ContactList />
     </>
-  )
+  );
 }
 
-export default App
